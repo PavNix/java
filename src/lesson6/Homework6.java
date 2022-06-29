7c8864de4b20741c8fe39cfb7d324632f2ecf2f8
Veronika <nika.zhevner1307@icloud.com>
2022-06-29 18:18:10 +0300

    29.06.2022

--- src/lesson6/Homework6.java
+++ src/lesson6/Homework6.java
@@ -6,7 +6,7 @@ public class Homework6 {
     public static void main(String[] args) {
         Homework6.task1();
         Homework6.task2("Hello my Java language");
-        Homework6.task3("ititititit");
+        Homework6.task3("oliolitryoliolioliolitryoliolioliolitryolioli");
     }
 
     public static void task1() {
@@ -42,24 +42,62 @@ public class Homework6 {
         System.out.println(maxWord);
     }
 
+    // public static void task3(String text) {
+    // String pattern = Character.toString(text.charAt(0));
+    // String buffer = "";
+
+    // int offset = 0;
+
+    // for (int i = 1; i < text.length(); i++) {
+    // String c = Character.toString(text.charAt(i));
+
+    // if (text.charAt(i) == pattern.charAt(offset)) {
+    // offset++;
+    // buffer += c;
+
+    // if (offset == pattern.length()) {
+    // break;
+    // }
+    // } else {
+    // offset = 0;
+    // pattern += buffer + c;
+    // buffer = "";
+    // }
+    // }
+
+    // System.out.println(pattern);
+    // }
+
     public static void task3(String text) {
-        String pattern = Character.toString(text.charAt(0));
 
-        int offset = 0;
+        String result = String.valueOf(text.charAt(0));
+        String tmp = "";
 
-        for (int i = 1; i < text.length(); i++) {
-            if (text.charAt(i) == pattern.charAt(offset)) {
-                offset++;
+        for (int i = 1, s = 0; i < text.length(); i++) {
+            if (s != 0) {
+                break;
+            }
+            for (int j = 0; j < text.length(); j += result.length()) {
+                tmp = text.substring(j, i + j);
 
-                if (offset == pattern.length()) {
+                if (!(result.equals(tmp))) {
+                    result += String.valueOf(text.charAt(i));
+                    if (0 != text.length() % result.length() && result.length() > text.length() / 2) {
+                        result += text.substring(result.length(), text.length());
+                        s++;
+                    }
                     break;
                 }
-            } else {
-                offset = 0;
-                pattern += Character.toString(text.charAt(i));
+
+                if (0 == text.length() % tmp.length() && text.length() - j == tmp.length()) {
+                    s++;
+                    break;
+                }
             }
         }
 
-        System.out.println(pattern);
+        System.out.println(result);
+
     }
+
 }
